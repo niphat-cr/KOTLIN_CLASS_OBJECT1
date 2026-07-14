cff802c17704fb1092004493ec06e0bd82bd5975
Niphat Craypo <niphat.cr@kmitl.ac.th>
2026-07-14 13:44:07 +0700

    unchanged main function

--- KOTLIN_CLASS_OBJECT1/src/Main.kt
+++ KOTLIN_CLASS_OBJECT1/src/Main.kt
@@ -1,3 +1,4 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -10,5 +11,6 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+
     }
 }
